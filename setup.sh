786816e2bb0c6fab4f7211b9421273a0fb6a369c
justindavid.ginn@gmail.com <Justindavid.ginn@gmail.com>
2020-10-11 13:57:06 -0500

    Automate vim/nvim setup

--- setup.sh
+++ setup.sh
@@ -1,5 +1,11 @@
-function config {
-   /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@
-}
-config checkout
-config config status.showUntrackedFiles no
+# Set upstream for config repo
+cd $HOME
+config branch --set-upstream-to origin master
+
+# Install VimPlug
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+# Install all vim plugins in .vimrc and nvim/init.vim
+vim +'PlugInstall --sync' +qa
+nvim +'PlugInstall --sync' +qa
