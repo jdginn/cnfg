8467883422dbba94ca8eedace1dcec292d0c4c5d
justindavid.ginn@gmail.com <Justindavid.ginn@gmail.com>
2021-06-08 12:47:21 -0500

    More wez updates

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,3 +1,7 @@
 local wezterm = require 'wezterm';
 
-  
+return {
+  color_scheme="Gruvbox Dark",
+  send_composed_key_when_left_alt_is_pressed=false,
+  send_composed_key_when_right_alt_is_pressed=false,
+}
